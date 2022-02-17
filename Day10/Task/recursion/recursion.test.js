2ce6c772b2460434091b1eb30dc758b284261f05
HamZa Akram Samha <smha.hmza@gmail.com>
2022-02-17 15:53:09 +0200

    recursion 1&2&3

--- Day10/Task/recursion/recursion.test.js
+++ Day10/Task/recursion/recursion.test.js
@@ -1,9 +1,9 @@
 /* Write a function to do the division operation without using the built-in division*/
 
 function division(number, dividedBy){
-    // Write you logic here.
-    console.log();
-    return;
+    if(dividedBy == 0)
+        return 0;
+    return number / dividedBy;
 }
 
 /* Write a function that implement Math.pow(x,n) but using recursion
@@ -12,10 +12,14 @@ pow(2,4) = 16
 */
 
 
-function pow(x,n){
-    // Write you logic here.
-    return;
-}
+function pow(x, n) {
+
+    if (n == 0)
+      return 1;
+      
+    return x * pow(x, n - 1);
+
+  }
 
 /* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
 the next integer is determined by summing the previous two. This gives us:
@@ -27,8 +31,11 @@ Write a function that take n as parameter and return the nth element in the Fibo
 Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
 
 function fibonacci(n){
-    // Write you logic here.
-    return;
+
+    if(n === 0) return 0;
+  	if(n === 1) return 1;
+  
+  	return fibonacci(n - 2) + fibonacci(n - 1);
 }
 
 /* Optional 
